675220ae96d5bf11c9ce4b6c175fd51903af2e83
Jacob Bradshaw <jakebradshaw123@gmail.com>
2020-11-05 13:55:03 -0800

    Added boilerplate ajax calls

--- script.js
+++ script.js
@@ -58,3 +58,36 @@
     cont.appendChild(sel_month);
     cont.appendChild(sel_day);
 }());
+
+
+//API Calls
+
+//OMBD
+var parameter = "A christmas story"
+var apiKey = "&apikey=578b0408"
+var queryURL = "http://www.omdbapi.com/?t=" + parameter + apiKey 
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      
+    console.log(response)
+
+  })
+
+//Calendarific
+var apiKey2 = "&api_key=7fcc61e9cff9ca714271b1579b4f3526db843694"
+var queryURL2 = "https://calendarific.com/api/v2/holidays?" + apiKey2 + "&country=US&year=2020"
+
+
+  $.ajax({
+    url: queryURL2,
+    method: "GET"
+  }).then(function(response) {
+      
+    console.log(response)
+
+  })
+
+  //End
